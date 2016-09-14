eae6c1506dc2f39ddc9f23d1715c7165a4e49bff
Gordon Weakliem <gordon.weakliem@gmail.com>
2016-09-13 19:17:54 -0600

    Added controller

---
+++ src/main/java/net/eightytwenty/HomeController.java
@@ -0,0 +1,15 @@
+package net.eightytwenty;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
