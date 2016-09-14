321021bdb9d11f03bb0191aa420efa9efdee6f89
Gordon Weakliem <gordon.weakliem@gmail.com>
2016-09-13 18:34:27 -0600

    Initial commit

---
+++ src/main/java/net/eightytwenty/RtdHackathonApplication.java
@@ -0,0 +1,12 @@
+package net.eightytwenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdHackathonApplication.class, args);
+	}
+}
