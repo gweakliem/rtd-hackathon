948470213c3e13b3d1954fc4c0fecfc6ac6eeb54
Gordon Weakliem <gordon.weakliem@gmail.com>
2016-09-13 20:07:20 -0600

    Added basic HTML form

--- src/main/java/net/eightytwenty/HomeController.java
+++ src/main/java/net/eightytwenty/HomeController.java
@@ -1,15 +1,17 @@
 package net.eightytwenty;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    String home(Model model) {
+        model.addAttribute("name", "foo");
+
+        return "index";
     }
 
 }
